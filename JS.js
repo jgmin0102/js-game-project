2f6a2dfe651cf5d0778489974a89907ba8226124
jgmin0102 <124388074+jgmin0102@users.noreply.github.com>
2023-02-24 00:01:12 +0900

    기본 장애물 생성

---
+++ JS.js
@@ -0,0 +1,21 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth - 100;
+canvas.height = window.innerHeight - 100;
+
+var dino = {
+    x : 10,
+    y : 200,
+    width : 50,
+    height : 50,
+    draw(){
+        ctx.fillStyle = 'green';
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+dino.draw()
+
+ctx.fillStyle = 'green';
+ctx.fillRect(120, 120, 100, 100);
